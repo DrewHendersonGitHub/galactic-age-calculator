5a7e8c8b0a228db948d8218c67291121f7486b73
Drew Henderson <drewhendersonemail@gmail.com>
2022-02-04 14:37:50 -0800

    added html form

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
-//import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-//import Calculator from './calculator.js';
+import Calculator from './calculator.js';
+
+$(document).ready(function() {
+  $('#calculator-form').submit(function(event) {
+    event.preventDefault();
+    let calculator = new Calculator($('#age').val());
+    $('#response').append("<p>" + calculator.convertMercury() + "</p>" + "<p>" + calculator.convertVenus() + "</p>" + "<p>" + calculator.convertMars() + "</p>" + "<p>" + calculator.convertJupiter() + "</p>");
+  });
+});
