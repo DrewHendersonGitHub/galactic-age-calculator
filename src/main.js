af0b73d09f3a35f3d7598f6ab3fd7228ac22ef3b
Drew Henderson <drewhendersonemail@gmail.com>
2022-02-04 17:00:33 -0800

    added styling

--- src/main.js
+++ src/main.js
@@ -10,8 +10,11 @@ $(document).ready(function() {
     event.preventDefault();
     calculator = new Calculator($('#age').val());
     if (calculator.age > 0) {
-      $('#calculator-response').html("<hr><p>Age in Mercury years: " + calculator.convertMercury() + "</p><p>Age in Venus years: " + calculator.convertVenus() + "<p>Age in Martian years: " + calculator.convertMars() + "<p>Age in Jupiter years: " + calculator.convertJupiter() + "</p>");
-      $('#life-expectancy-form').show();
+      $('#calculator-response').html("<hr><p>Age in Mercury years: " + calculator.convertMercury() + "</p><p>Age in Venus years: " + calculator.convertVenus() + "<p>Age in Martian years: " + calculator.convertMars() + "<p>Age in Jupiter years: " + calculator.convertJupiter() + "</p><button id='calculator-response-ask'>Calculate life expectancy?</button>");
+      $("#calculator-response-ask").on("click", function() {
+        $('#life-expectancy-form').show();
+        $('#calculator-response-ask').hide();
+      });
     }
     else {
       $('#calculator-response').html("<p>Please enter a valid age</p><hr>");
@@ -21,7 +24,11 @@ $(document).ready(function() {
   
   $('#life-expectancy-form').submit(function(event) {
     event.preventDefault();
-    $('#life-expectancy-response').html("<hr><p>Your life expectancy is " + calculator.lifeExpectancy($('#exercise').val(), $('#veggies').val(), $('#gender').val(), $('#smoker').val()) + " Earth years.</p>");
+    let expectancy = calculator.lifeExpectancy($('#exercise').val(), $('#veggies').val(), $('#gender').val(), $('#smoker').val());
+    $('#life-expectancy-response').html("<hr><p>Your life expectancy is " + expectancy + " Earth years.</p>");
+    if (expectancy < calculator.age) {
+      $('#life-expectancy-response').append("<p>You have lived " + (calculator.age - expectancy) + " years over your calculated lifetime length.</p>");
+    }
     $('#life-expectancy-form').hide();
     $('#life-expectancy-response').show();
   });
